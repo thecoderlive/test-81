034138df0fa134d99bc38247221eb58912a281f4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-02 13:38:22 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1c6cea9a-c551-4211-be23-6be2c49ed3ff",
+    "home_feed": [
+        {
+            "id": "f78f6086-4e4c-4ee3-9c2e-f7cc7c736c19",
+            "artist_name": "traditional",
+            "home_carousel": [
+                {
+                    "id": "8b1bc265-f429-4bb8-a73b-fec779fab241",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                },
+                {
+                    "id": "82eb7a52-b3fd-4597-8254-69c89bdcaec2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "f3e2feba-41d9-46c8-821d-f5f9270cf067",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "3272129c-76e3-47a6-b4ee-c83ea6400985",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "da5d6316-6836-4bbf-9703-c5a461e87432",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/75.png"
+                }
+            ],
+            "art_caption": "affect",
+            "art_description": "Democratic physical community soldier. Step require only thousand. Ever sure yourself present."
+        },
+        {
+            "id": "839e168d-548a-4e9d-a903-db82b9d33e6d",
+            "artist_name": "play",
+            "home_carousel": [
+                {
+                    "id": "91769a28-106f-430c-a3b4-55705cc8a437",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "9eb92ed7-b7a6-4639-a409-a178834f5d5d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "56959d21-e530-404a-bd1c-738d2897a8b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "af669ff8-ddf0-4996-82c7-6f3ef32588ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png"
+                },
+                {
+                    "id": "852e12eb-b96b-409b-9f5d-b758482095af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                }
+            ],
+            "art_caption": "arrive",
+            "art_description": "End break recent fight thus. Various push company ball feeling. Experience skill manage but."
+        },
+        {
+            "id": "07f494f1-d324-4b04-8a0d-1fd4d9bd918b",
+            "artist_name": "buy",
+            "home_carousel": [
+                {
+                    "id": "f2e3e74c-1f6d-4788-a256-407fe5c7168a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "814b2e7e-ac19-4dd8-a4a5-10e605f5f189",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "804a5561-afd1-4a05-b949-3f02a994af52",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "978b4050-0962-42b4-bf04-a3dd61e2f02b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "1a07e36c-7b17-4a38-9b8e-f15e8f25a020",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                }
+            ],
+            "art_caption": "each",
+            "art_description": "Exist case particularly theory key probably improve keep. Sort sense head including single."
+        },
+        {
+            "id": "8ab3b376-3e7f-4fa1-a367-348867ebb5ee",
+            "artist_name": "threat",
+            "home_carousel": [
+                {
+                    "id": "11433a4b-fea6-44e2-9b73-a5b44dcbc6be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                },
+                {
+                    "id": "bfaa4ace-7aee-44a1-aa6c-afa3629eec56",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "c8747209-92d3-4449-ab83-4f04d64cbd6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "204369f3-f343-4369-a7c1-04d63bc03c35",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "f08fa7f9-ae9c-4f63-8ff5-d4c79500d7b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                }
+            ],
+            "art_caption": "condition",
+            "art_description": "Past fly everybody cold him type among. Whatever less its camera seem rather actually under. Answer why pass agree drive. Interview audience line magazine her serious."
+        },
+        {
+            "id": "565b6ce7-5ea5-468e-a2f6-5052adc14a08",
+            "artist_name": "fund",
+            "home_carousel": [
+                {
+                    "id": "24049c5a-23e0-49d7-9a63-55885f97c735",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "7a5c07bb-df45-4ae4-827a-12dc15e7c0cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                },
+                {
+                    "id": "2593620b-55d1-46e4-9ab9-843e0248a784",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/75.png"
+                },
+                {
+                    "id": "698c6135-cf8c-4833-8697-1b75673d62b9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "060c56a1-c8ea-47de-828c-8549044804ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                }
+            ],
+            "art_caption": "along",
+            "art_description": "Police represent page admit season consumer. Him air worry wife if attention."
+        }
+    ]
+}
